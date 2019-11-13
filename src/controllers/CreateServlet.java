ec95981b7223ffe6d8fbc0d55eae0a7ff3891c49
akio.fumimoto <jajmtpatj@gmail.com>
2019-11-13 22:21:04 +0900

    MessageValidato

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -40,8 +40,6 @@ public class CreateServlet extends HttpServlet {
 
             Tasks t = new Tasks();
 
-            String title = request.getParameter("title");
-            t.setTitle(title);
 
             String content = request.getParameter("content");
             t.setContent(content);
--- src/models/validators/MessageValidator.java
+++ src/models/validators/MessageValidator.java
@@ -10,10 +10,6 @@ public class MessageValidator {
     public static List<String> validate(Tasks m) {
         List<String> errors = new ArrayList<String>();
 
-        String title_error = _validateTitle(m.getTitle());
-        if(!title_error.equals("")) {
-            errors.add(title_error);
-        }
 
         String content_error = _validateContent(m.getContent());
         if(!content_error.equals("")) {
@@ -23,19 +19,12 @@ public class MessageValidator {
         return errors;
     }
 
-    // タイトルの必須入力チェック
-    private static String _validateTitle(String title) {
-        if(title == null || title.equals("")) {
-            return "タイトルを入力してください。";
-        }
 
-        return "";
-    }
 
-    // メッセージの必須入力チェック
+    // タスクの必須入力チェック
     private static String _validateContent(String content) {
         if(content == null || content.equals("")) {
-            return "メッセージを入力してください。";
+            return "タスクを入力してください。";
         }
 
         return "";
